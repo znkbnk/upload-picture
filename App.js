a2acafc1fe78fc515fc62925d776d489bb5d514b
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-12 19:50:33 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+//App.js
+import React from 'react';
+import FileUploader from './FileUploader';
+
+const App = () => {
+  return (
+    <div>
+      <h1>File Uploader</h1>
+      <FileUploader />
+    </div>
+  );
+};
+
+export default App;
